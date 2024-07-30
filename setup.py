df24100122a77ed9b15a4fb38cccfa4a4c60fdb8
Abdisa Merga <abdisamerga@yahoo.com>
2024-07-30 11:10:37 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,8 @@ def install():
     "wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb && sudo apt-get install --assume-yes ./chrome-remote-desktop_current_amd64.deb",
     "sudo apt install --assume-yes xfce4 desktop-base dbus-x11 xscreensaver",
     "sudo apt install xfce4-terminal -y",
-    "sudo apt install firefox-esr -y",
+    "wget -O firefox.tar.bz2 "https://download.mozilla.org/?product=firefox-latest&os=linux64&lang=en-US",
+    "tar xjf firefox.tar.bz2 -C ~/",
     "sudo apt-get install geany -y",
     "sudo apt-get install vim-gtk3 -y",
     "sudo apt install iputils-ping -y"
